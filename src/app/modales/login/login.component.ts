27c2da419d298727a86c7eaba323f9be1384379a
Agustin Gomez <agustin_0897@hotmail.com>
2023-10-21 13:54:00 -0300

    login

--- src/app/modales/login/login.component.ts
+++ src/app/modales/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   hide = true;
+  
+  constructor(private dialogRef: MatDialogRef<LoginComponent>) {}
+  
+  closeModal() {
+    this.dialogRef.close();
+  }
 }
