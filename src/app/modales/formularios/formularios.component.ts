5d8e3083b1bae22351202f3da1f02d7dd7928467
LadyCari <k_roferraris@hotmail.com>
2023-11-07 20:49:56 -0300

    formulario agregado cuadro y estica pop up, falta vincular datos

--- src/app/modales/formularios/formularios.component.ts
+++ src/app/modales/formularios/formularios.component.ts
@@ -1,5 +1,7 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { lastValueFrom } from 'rxjs';
 import { Formulario } from 'src/app/entidades/model-implements';
 import { HttpService } from 'src/app/servicios/http/http.service';
@@ -11,10 +13,23 @@ import { Urls } from 'src/app/url-globales';
   styleUrls: ['./formularios.component.css']
 })
 export class FormulariosComponent implements OnInit{
+
   formularios: Formulario[] = [];
+  displayedColumns: string[] = ['id', 'nombre', 'e-mail', 'estado', 'eliminar'];
+  dataSource = new MatTableDataSource<any>([]);
+  resultsLength = 0;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   constructor(@Inject(MAT_DIALOG_DATA) public data: string, private http: HttpService) { }
+
   ngOnInit(): void {
     this.cargarDatosFormularios(this.data);
+    this.dataSource.paginator = this.paginator;
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
   private async cargarDatosFormularios(email: string) {
